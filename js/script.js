09cdd517eb8c51f149377b5f5db957ab2833f2bd
Jhon Angulo <jhonmanuelm89@gmail.com>
2020-06-05 14:49:19 -0500

    slide with the keys

--- js/script.js
+++ js/script.js
@@ -1,9 +1,16 @@
-console.log('a')
-
+document.onkeydown = checkKey
 const item_1 = document.getElementById('item_1')
 const item_2 = document.getElementById('item_2')
 
 function handleChange() {
   item_1.classList.toggle('hide')
   item_2.classList.toggle('hide')
+}
+
+function checkKey(e) {
+  console.log(e.keyCode)
+
+  if (e.keyCode === 37 || e.keyCode === 39) {
+    handleChange()
+  }
 }
