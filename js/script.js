51083a1f064df477cbc15004fc11f5de5378dc79
Jhon Angulo <jhonmanuelm89@gmail.com>
2020-06-04 20:20:45 -0500

    javascript interactions

---
+++ js/script.js
@@ -0,0 +1,9 @@
+console.log('a')
+
+const item_1 = document.getElementById('item_1')
+const item_2 = document.getElementById('item_2')
+
+function handleChange() {
+  item_1.classList.toggle('hide')
+  item_2.classList.toggle('hide')
+}
